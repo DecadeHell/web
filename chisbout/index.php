014bcaf1ab221d4cbe6d8eab488ed6d900c3ab4f
Chí Bảo <107165352+DecadeHell@users.noreply.github.com>
2022-10-27 01:59:55 +0700

    hihi

--- chisbout/index.php
+++ chisbout/index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 $urladmin ="http://localhost:8080/chisbout/admin/";
-$urluser ="http://localhost:8080/chisbout/";
+$urluser ="postgres://smbabbkpimgkyy:18ac43a89440015e2db19446deb76475c5d43cca4d49582c4b1091333f134eaa@ec2-3-213-66-35.compute-1.amazonaws.com:5432/d5b9l38odctuik";
 $home = "home.php";
 $register = "register.php";
 $shoppingcart = "shoppingcart.php";

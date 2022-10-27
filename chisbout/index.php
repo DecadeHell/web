a09c8d762e7a782b0094ba60e9782ec56dd32228
Chí Bảo <107165352+DecadeHell@users.noreply.github.com>
2022-10-27 08:19:15 +0700

    Update index.php

--- chisbout/index.php
+++ chisbout/index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-$urladmin ="http://localhost:8080/chisbout/admin/";
-$urluser ="postgres://smbabbkpimgkyy:18ac43a89440015e2db19446deb76475c5d43cca4d49582c4b1091333f134eaa@ec2-3-213-66-35.compute-1.amazonaws.com:5432/d5b9l38odctuik";
+//$urladmin ="http://localhost:8080/chisbout/admin/";
+//$urluser ="http://localhost:8080/chisbout/";
 $home = "home.php";
 $register = "register.php";
 $shoppingcart = "shoppingcart.php";
@@ -13,6 +13,6 @@ $host = "localhost";
 $username="root";
 $password="";
 $db= "myshop";
-$conn = mysqli_connect($host,$username,$password,$db) or die("Can not connect database ".mysqli_connect_error());
+$conn = pg_connect('postgres://pznogisegeuiqr:d42586754f2dd097de79d4e31a01daadedbc24696a71b9ceb66bfd2926117774@ec2-44-198-82-71.compute-1.amazonaws.com:5432/df001mhbc8v335');
 
 include('theme.php');
